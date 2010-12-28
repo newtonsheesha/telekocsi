a9f373e68d356a2c8c3f8da42c424753234ce4fd
bbelin.sigal@gmail.com <bbelin.sigal@gmail.com@86feb3f9-bbda-ba12-cbf9-cd0f5b4cd948>
2010-12-28 21:28:49 +0000

    IHM : implementation logique metier

--- trunk/TELEKOCSI-ANDROID/src/com/alma/telekocsi/Transaction.java
+++ trunk/TELEKOCSI-ANDROID/src/com/alma/telekocsi/Transaction.java
@@ -2,10 +2,16 @@ package com.alma.telekocsi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class Transaction extends Activity {
 	
+	OnClickListener onClickListener = null;
+	Button btValider;
+	Button btAnnuler;
 	
     /** Called when the activity is first created. */
     @Override
@@ -18,6 +24,39 @@ public class Transaction extends Activity {
          * ----------------------------- 
          */
          setContentView(R.layout.transaction);
+
+         btValider = (Button)findViewById(R.id.btTransactionValider);
+         btValider.setOnClickListener(getOnClickListener()); 
+         
+         btAnnuler = (Button)findViewById(R.id.btTransactionAnnuler);
+         btAnnuler.setOnClickListener(getOnClickListener()); 
+    }
+    
+    
+    public OnClickListener getOnClickListener() {
+    	
+    	if (onClickListener == null) {
+    		onClickListener = new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					
+					if (v == btAnnuler) {
+						finish();
+					} else if (v == btValider) {
+						actionValider();
+						finish();
+					}
+				}
+			};
+    	}
+    		
+    	return onClickListener;
+    }
+ 
+    
+    private void actionValider() {
+    	
     }
 	
 }
