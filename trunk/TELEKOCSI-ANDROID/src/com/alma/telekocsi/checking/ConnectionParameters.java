b0f80f990807f7d9b93ed73a5dc4260a28806019
groozze <groozze@86feb3f9-bbda-ba12-cbf9-cd0f5b4cd948>
2010-12-30 11:29:39 +0000

    verification des infos entrees

---
+++ trunk/TELEKOCSI-ANDROID/src/com/alma/telekocsi/checking/ConnectionParameters.java
@@ -0,0 +1,16 @@
+package com.alma.telekocsi.checking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConnectionParameters extends Activity {
+	
+	private static final String emailPattern = "[a-zA-Z0-9_-.]+@[a-zA-Z]+\\.[a-zA-Z]{2,3}";
+	
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+		 
+	        super.onCreate(savedInstanceState);
+	 }
+
+}
