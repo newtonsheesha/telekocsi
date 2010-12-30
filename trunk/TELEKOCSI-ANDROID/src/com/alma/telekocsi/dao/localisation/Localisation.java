21b1fd46de48834e0fab36be162f58920818bd21
bbelin.sigal@gmail.com <bbelin.sigal@gmail.com@86feb3f9-bbda-ba12-cbf9-cd0f5b4cd948>
2010-12-30 18:24:53 +0000

    Double au lieu de double pour les Localisations

--- trunk/TELEKOCSI-ANDROID/src/com/alma/telekocsi/dao/localisation/Localisation.java
+++ trunk/TELEKOCSI-ANDROID/src/com/alma/telekocsi/dao/localisation/Localisation.java
@@ -7,8 +7,8 @@ public class Localisation {
 	
 	private String idProfil;
 	private String pointGPS;
-	private double longitude;
-	private double latitude;	
+	private Double longitude;
+	private Double latitude;	
 	private String dateLocalisation;
 	private String heureLocalisation;
 	
@@ -53,19 +53,19 @@ public class Localisation {
 		this.heureLocalisation = heureLocalisation;
 	}
 
-	public double getLongitude() {
+	public Double getLongitude() {
 		return longitude;
 	}
 
-	public void setLongitude(double longitude) {
+	public void setLongitude(Double longitude) {
 		this.longitude = longitude;
 	}
 
-	public double getLatitude() {
+	public Double getLatitude() {
 		return latitude;
 	}
 
-	public void setLatitude(double latitude) {
+	public void setLatitude(Double latitude) {
 		this.latitude = latitude;
 	}	
 	
