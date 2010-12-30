94e391194cc7024cbbab7596a7722ca76bfd59b1
rvesteg <rvesteg@86feb3f9-bbda-ba12-cbf9-cd0f5b4cd948>
2010-12-30 14:14:21 +0000

    Implementation de la session en tant que service.
    La première activité qui se lance devra faire.
    Dans le onCreate.
    
    SessionFactory.init(this);

--- trunk/TELEKOCSI-ANDROID/src/com/alma/telekocsi/session/SessionImpl.java
+++ trunk/TELEKOCSI-ANDROID/src/com/alma/telekocsi/session/SessionImpl.java
@@ -7,30 +7,122 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Binder;
+import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.alma.telekocsi.R;
 import com.alma.telekocsi.dao.profil.Profil;
 import com.alma.telekocsi.dao.profil.ProfilDAO;
 
 /**
- * @author Fatima
+ * @author Rv
  *
  */
-public class SessionImpl implements Session {
-
+public class SessionImpl extends IntentService implements Session {
+	/**
+     * Class for clients to access.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with
+     * IPC.
+     */
+    public class LocalBinder extends Binder {
+        SessionImpl getService() {
+            return SessionImpl.this;
+        }
+    }
+	
+	
+	public static final String KEY_PROFILE_ID = "profile_id";
+	private static final String PREFERENCES_STORE = "TELEKOCSI_PREFERENCES";
+	
+	/**
+	 * settings
+	 */
+	private SharedPreferences settings;
+	/**
+	 * The notification manager
+	 */
+	private NotificationManager notificationMgr;
 	/**
 	 * Liste avec gestion des accès concurrents
 	 */
 	private final List<SessionListener> listeners 
 		= Collections.synchronizedList(new ArrayList<SessionListener>());
-	private final ProfilDAO profileDAO = new ProfilDAO();
-	private Profil profile = null;
 	
+	/**
+	 * The active profile
+	 */
+	private Profil profile = null;
+
+	/**
+	 * The binder
+	 */
+	private LocalBinder mBinder = new LocalBinder();
+	
+	//DAO
+	private final ProfilDAO profileDAO = new ProfilDAO();
+
+	/**
+	 * 
+	 * @param name
+	 */
+    public SessionImpl(String name) {
+		super(name);
+	}
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("Session", "Received start id " + startId + ": " + intent);
+        // We want this service to continue running until it is explicitly
+        // stopped, so return sticky.
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        // Cancel the persistent notification.
+        notificationMgr.cancel(R.string.session_service_started);
+
+        // Tell the user we stopped.
+        Toast.makeText(this, R.string.session_service_stopped, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		notificationMgr = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);		
+		settings = getSharedPreferences(PREFERENCES_STORE, Context.MODE_PRIVATE);
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+	}
+
+
 	/* (non-Javadoc)
 	 * @see com.alma.telekocsi.session.Session#getActiveProfile()
 	 */
 	@Override
-	public Profil getActiveProfile(){
+	public synchronized Profil getActiveProfile(){
+		if(profile==null){
+			//On charge le profil
+			String id = settings.getString(KEY_PROFILE_ID, null);
+			if(id!=null){
+				profile = profileDAO.getProfil(id);
+			}
+		}
 		return profile;
 	}
 
@@ -38,10 +130,13 @@ public class SessionImpl implements Session {
 	 * @see com.alma.telekocsi.session.Session#saveProfile(com.alma.telekocsi.dao.profil.Profil)
 	 */
 	@Override
-	public void saveProfile(Profil profile) {
+	public synchronized void saveProfile(Profil profile) {
 		if(profile!=null){
 			profileDAO.insert(profile);
 			this.profile = profile;
+			Editor editor = settings.edit();
+			editor.putString(KEY_PROFILE_ID, profile.getId());
+			editor.commit();
 		}
 	}
 
@@ -61,7 +156,7 @@ public class SessionImpl implements Session {
 	@Override
 	public void removeSessionListener(SessionListener listener) {
 		if(listener!=null){
-			listeners.add(listener);
+			listeners.remove(listener);
 		}
 	}
 
@@ -80,5 +175,6 @@ public class SessionImpl implements Session {
 			}
 		}
 	}
+
 	
 }
