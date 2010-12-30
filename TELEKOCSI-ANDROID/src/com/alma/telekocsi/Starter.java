da04944cd5c9351d1a69166aa92ec42985c8477b
groozze <groozze@86feb3f9-bbda-ba12-cbf9-cd0f5b4cd948>
2010-12-30 14:17:35 +0000

    emboitement des activites

--- TELEKOCSI-ANDROID/src/com/alma/telekocsi/Starter.java
+++ TELEKOCSI-ANDROID/src/com/alma/telekocsi/Starter.java
@@ -1,94 +1,35 @@
 package com.alma.telekocsi;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import com.alma.telekocsi.checking.ConnectionParameters;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class Starter extends Activity {
 	
-	private OnClickListener onClickListener = null;
-	private Button profileSettingsButton;
-	private EditText email;
-	private EditText password1;
-	private EditText password2;
-	
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
         
+        System.out.println("nawak-starter");
+
         boolean exist = false;
         boolean disconnected = true;
         //le compte de la personne n'est pas enregistre
         if(!exist){
-            setContentView(R.layout.connection_parameters);
-            profileSettingsButton = (Button)findViewById(R.id.profile_settings_button);
-            profileSettingsButton.setOnClickListener(getOnClickListener());
-            email = (EditText)findViewById(R.id.connection_parameters_email);
-            password1 = (EditText)findViewById(R.id.connection_parameters_password1);
-            password2 = (EditText)findViewById(R.id.connection_parameters_password2);
+        	startActivity(new Intent(this, ConnectionParameters.class));
         }
         //sinon savoir s'il s'etait deconnecte ou non
         else if(disconnected){
-        	//si deconnecte, on affiche l'ecran de connexion
-        	setContentView(R.layout.identification);
+        	startActivity(new Intent(this, Identification.class));
         }
         else{
         	//sinon on affiche l'ecran principal
-        	setContentView(R.layout.main_menu);
+        	startActivity(new Intent(this, MainMenu.class));
         }
+        
+        finish();
     }
     
-    protected OnClickListener getOnClickListener(){
-    	if(onClickListener==null){
-    		onClickListener = makeOnClickListener();
-    	}
-    	return onClickListener;
-    }
     
-    protected OnClickListener makeOnClickListener(){
-    	return new OnClickListener(){
-
-			@Override
-			public void onClick(View v) {
-				if(v==profileSettingsButton){
-					startProfileSettings();
-				}
-			}
-    		
-    	};
-    }
-    
-    private static final String emailPattern = "^[\\w-\\.]+@[a-zA-Z]+\\.[a-zA-Z]{2,3}$";
-    
-    private void startProfileSettings(){
-    	
-    	Pattern p = Pattern.compile(Starter.emailPattern);
-    	Matcher m = p.matcher(email.getText().toString());
-    	
-    	if(!m.find()){
-    		email.setTextColor(Color.RED);
-    	}
-    	else{
-    		if(!password1.getText().toString().equals(password2.getText().toString())
-    				|| password1.getText().toString().equals("")){
-        		password1.setTextColor(Color.RED);
-        		password2.setTextColor(Color.RED);
-    		}
-    		else{
-        		Intent intent = new Intent(this, ProfileSettings.class);
-        		startActivity(intent);
-    		}
-    	}
-    }
-
 }
