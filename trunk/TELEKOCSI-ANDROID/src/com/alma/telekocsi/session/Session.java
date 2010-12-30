eb54abf1dc64d018a50fde3d5853cbcaf05c06be
rvesteg <rvesteg@86feb3f9-bbda-ba12-cbf9-cd0f5b4cd948>
2010-12-30 12:35:28 +0000

    Ajout d' une session comme facade pour un accès commun des données. Donnant un point d'optimization sans avoir modifier toutes les vues. La session apporte aussi un gestionnaire d'évèmenent.
    Voilà un example d'utilisation en début de programme.
    
    Session session = SessionFactory.getCurrentSession();
    Profil profile = session.getActiveProfile();
    if(profile==null){
       profile = //Créér profile
       session.saveProfile(profile);
    }
    //Continuer

---
+++ trunk/TELEKOCSI-ANDROID/src/com/alma/telekocsi/session/Session.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.alma.telekocsi.session;
+
+import com.alma.telekocsi.dao.profil.Profil;
+
+/**
+ * @author Rv
+ * Contenir les informations relatifs a une session
+ * d'utilisation et fournir un point d'entrer pour la notification.
+ */
+public interface Session {
+
+	/**
+	 * Accéder au profil courant.
+	 * On vérifie que le profil n'a pas encore été crée, si oui on le charge
+	 * @return Le profil actif et <code>null</code> si non existant
+	 */
+	public abstract Profil getActiveProfile();
+	
+	/**
+	 * Remplacer ou créer le profil actif
+	 * @param profile Le nouveau profil
+	 */
+	public abstract void saveProfile(Profil profile);
+	
+	/**
+	 * Ecouter les l'évènement sur la session
+	 * @param listener
+	 */
+	public abstract void addSessionListener(SessionListener listener);
+	
+	/**
+	 *  Arrêter l'écoute
+	 * @param listener
+	 */
+	public abstract void removeSessionListener(SessionListener listener);
+}
