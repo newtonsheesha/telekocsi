3c859019a4eb60e60abc911978ee874d406bc273
groozze <groozze@86feb3f9-bbda-ba12-cbf9-cd0f5b4cd948>
2010-12-31 00:05:04 +0000

    nouveau menu + lien avec les transactions de nobru

---
+++ TELEKOCSI-ANDROID/src/com/alma/telekocsi/DriverTab.java
@@ -0,0 +1,61 @@
+package com.alma.telekocsi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class DriverTab extends Activity {
+	
+	private OnClickListener onClickListener = null;
+	private Button routeActivationButton;
+	private Button driverTransactionButton;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.onglet_conducteur);
+
+		routeActivationButton = (Button)findViewById(R.id.route_activation_button);
+		routeActivationButton.setOnClickListener(getOnClickListener());
+		
+		driverTransactionButton = (Button)findViewById(R.id.driver_transaction_button);
+		driverTransactionButton.setOnClickListener(getOnClickListener());
+	}
+	
+	private OnClickListener getOnClickListener(){
+		if(onClickListener==null){
+			onClickListener = makeOnClickListener();
+		}
+		return onClickListener;
+	}
+	
+	private OnClickListener makeOnClickListener(){
+		return new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				if(v==routeActivationButton){
+					startActivation();
+				}
+				else if(v==driverTransactionButton){
+					startTransaction();
+				}
+			}
+			
+		};
+	}
+	
+	private void startActivation(){
+		
+	}
+	
+	private void startTransaction(){
+		Intent intent = new Intent(this, Transaction.class);
+		startActivity(intent);
+	}
+
+}
--- TELEKOCSI-ANDROID/src/com/alma/telekocsi/MainMenu.java
+++ TELEKOCSI-ANDROID/src/com/alma/telekocsi/MainMenu.java
@@ -1,55 +1,48 @@
 package com.alma.telekocsi;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MainMenu extends Activity {
-	
-	private OnClickListener onClickListener = null;
-	private Button routeActivationButton;
-	private Button routeSearchButton;
+public class MainMenu extends TabActivity {
+
+	private TabHost tabHost;
+	private TabSpec tabSpec;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
-    
-		setContentView(R.layout.main_menu);
-		
-		routeActivationButton = (Button)findViewById(R.id.route_activation_button);
-		routeActivationButton.setOnClickListener(getOnClickListener());
-		
-		routeSearchButton = (Button)findViewById(R.id.route_search_button);
-		routeSearchButton.setOnClickListener(getOnClickListener()); 
-	}
-	
-	private OnClickListener getOnClickListener(){
-		if(onClickListener==null){
-			onClickListener = makeOnClickListener();
-		}
-		return onClickListener;
-	}
-	
-	private OnClickListener makeOnClickListener(){
-		return new OnClickListener(){
-
-			@Override
-			public void onClick(View v) {
-				if(v==routeSearchButton){
-					startSearch();
-				}
-			}
-			
-		};
-	}
-	
-	private void startSearch(){
-		Intent intent = new Intent(this, TrajetRecherche.class);
-		startActivity(intent);
-	}
-
+        setContentView(R.layout.main_menu);
+        
+        tabHost = getTabHost();
+        Intent intent = new Intent().setClass(this, DriverTab.class);
+        tabSpec = tabHost.newTabSpec("buildings").setIndicator("Conducteur",getResources().getDrawable(R.drawable.volant)).setContent(intent);
+        tabHost.addTab(tabSpec);
+     
+        intent = new Intent().setClass(this, OccupantTab.class);
+        tabSpec = tabHost.newTabSpec("research").setIndicator("Passager",getResources().getDrawable(R.drawable.autostop)).setContent(intent);
+        tabHost.addTab(tabSpec);
+     
+//        tabHost.setCurrentTab(0);
+        
+//        Intent intent = new Intent(this, ActiviteTab.class);
+//        tabHost = getTabHost();
+//        
+//        intent.putExtra("valeur","hi");
+//        
+//        tabSpec = tabHost.newTabSpec("conducteur").setIndicator("Conducteur", 
+//        		getResources().getDrawable(R.drawable.volant)).setContent(intent);
+//        tabHost.addTab(tabSpec);
+//        
+//        
+//        intent.putExtra("valeur", "Hello world !");
+//        
+//        tabSpec = tabHost.newTabSpec("passager").setIndicator("Passager", 
+//        		getResources().getDrawable(R.drawable.autostop)).setContent(intent);
+//        tabHost.addTab(tabSpec);
+        
+    }
 }
