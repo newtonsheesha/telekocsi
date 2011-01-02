a803df3bd91c1aadaa0082bf700787b9f5902b37
groozze <groozze@86feb3f9-bbda-ba12-cbf9-cd0f5b4cd948>
2011-01-02 11:57:38 +0000

    debut dimplementation de loptions menu (kan on clique sur le bouton menu du phone)

--- TELEKOCSI-ANDROID/src/com/alma/telekocsi/OptionsMenu.java
+++ TELEKOCSI-ANDROID/src/com/alma/telekocsi/OptionsMenu.java
@@ -2,19 +2,11 @@ package com.alma.telekocsi;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class OptionsMenu extends Activity {
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
