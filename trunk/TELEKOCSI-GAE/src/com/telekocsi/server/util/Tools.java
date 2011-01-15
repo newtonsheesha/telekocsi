f6141a10d58d9154c4331533253f4e452f44664d
bbelin.sigal@gmail.com <bbelin.sigal@gmail.com@86feb3f9-bbda-ba12-cbf9-cd0f5b4cd948>
2011-01-15 20:51:50 +0000

    - login : actualise l'attribut connecte cote serveur - Trajet : 3 etats / dispo / actif / fin

--- trunk/TELEKOCSI-GAE/src/com/telekocsi/server/util/Tools.java
+++ trunk/TELEKOCSI-GAE/src/com/telekocsi/server/util/Tools.java
@@ -1,14 +1,21 @@
 package com.telekocsi.server.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class Tools {
 
+	
 	public static final boolean LOCAL = true;
 	public static final String BASE_URL_LOCAL = "http://localhost:8888";
 	public static final String BASE_URL_DISTANT = "http://alma-telekocsi.appspot.com";
+	
+	public static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	private static final EntityManagerFactory ENTITY_MANAGER = 
 		Persistence.createEntityManagerFactory("transactions-optional");
@@ -25,4 +32,21 @@ public class Tools {
 			return BASE_URL_DISTANT + path;
 	}
 	
+	
+	/**
+	 * Recuperation d'un objet Date
+	 * @param date au format "jj/MM/aaaa"
+	 * @return java.util.Date
+	 */
+	public static Date getDate(String date) {
+	
+		Date dateRef = null;
+		try {
+			dateRef = sdf.parse(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return dateRef;
+	}
 }
